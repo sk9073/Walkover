dc216e78fac619fe6737d0072cd3e45095431d04
sivanandi <59682017+sivanandi@users.noreply.github.com>
2022-05-01 21:48:18 +0530

    Added Card component to display list of works

---
+++ frontend/components/blog/Card.js
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import renderHTML from 'react-render-html';
+import moment from 'moment';
+import { API } from '../../config';
+
+const Card = ({ blog }) => {
+
+    return (
+        <div className="lead pb-4">
+            <header>
+                <Link href={`/blogs/${blog.slug}`}>
+                    <a>
+                        <h2 className="pt-3 pb-3 font-weight-bold">{blog.title}</h2>
+                    </a>
+                </Link>
+            </header>
+            <section>
+                <p className="mark ml-1 pt-2 pb-2">
+                    Written by{' '}
+                    <Link href={`/profile/${blog.postedBy}`}>
+                        <a>{blog.postedBy}</a>
+                    </Link>{' '}
+                    | Published {moment(blog.updatedAt).fromNow()}
+                </p>
+            </section>
+
+            <div className="row">
+                <div className="col-md-12">
+                    <section>
+                        <div className="pb-3">{renderHTML(blog.excerpt)}</div>
+                        <Link href={`/blogs/${blog.slug}`}>
+                            <a className="btn btn-primary pt-2">Read more</a>
+                        </Link>
+                    </section>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
