085f7665a0d7bcaff9544258408a85a0b4f89908
sivanandi <59682017+sivanandi@users.noreply.github.com>
2022-05-01 19:38:33 +0530

    Created Control function in frontend for Creating Work , Listing Works , Getting a single Work

---
+++ frontend/actions/auth.js
@@ -0,0 +1,24 @@
+import cookie from 'js-cookie';
+
+
+export const setCookie = (key, value) => {
+    if (process.browser) {
+        cookie.set(key, value, {
+            expires: 1
+        });
+    }
+};
+
+export const removeCookie = key => {
+    if (process.browser) {
+        cookie.remove(key, {
+            expires: 1
+        });
+    }
+};
+// get cookie
+export const getCookie = key => {
+    if (process.browser) {
+        return cookie.get(key);
+    }
+};
