4c07c4f7826291f8e74a1840c9db2c501016f763
sivanandi <59682017+sivanandi@users.noreply.github.com>
2022-05-01 20:55:39 +0530

    Added CRUD Components for basic Functionalities on Work

---
+++ frontend/components/crud/BlogCreate.js
@@ -0,0 +1,129 @@
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import Router from 'next/router';
+import dynamic from 'next/dynamic';
+import { withRouter } from 'next/router';
+import { getCookie} from '../../actions/auth';
+import { createBlog } from '../../actions/blog';
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+import '../../node_modules/react-quill/dist/quill.snow.css';
+import { QuillModules, QuillFormats } from '../../helpers/quill';
+
+const CreateBlog = ({ router }) => {
+    const blogFromLS = () => {
+        if (typeof window === 'undefined') {
+            return false;
+        }
+
+        if (localStorage.getItem('blog')) {
+            return JSON.parse(localStorage.getItem('blog'));
+        } else {
+            return false;
+        }
+    };
+
+    const [body, setBody] = useState(blogFromLS());
+    const [values, setValues] = useState({
+        error: '',
+        sizeError: '',
+        success: '',
+        formData: '',
+        title: '',
+        hidePublishButton: false
+    });
+
+    const { error, sizeError, success, formData, title, hidePublishButton } = values;
+    const token = getCookie('token');
+
+    useEffect(() => {
+        setValues({ ...values, formData: new FormData() });
+    }, [router]);
+
+    const publishBlog = e => {
+        e.preventDefault();
+        // console.log('ready to publishBlog');
+        createBlog(formData , token).then(data => {
+            console.log(data);
+            if (data.error) {
+                console.log(data);
+                setValues({ ...values, error: data.error });
+            } else {
+                console.log("heyy");
+
+                setValues({ ...values, title: '', error: '', success: `A new blog titled "${data.title}" is created` });
+                setBody('');
+            }
+        });
+    };
+
+    const handleChange = name => e => {
+        // console.log(e.target.value);
+        const value = name === 'photo' ? e.target.files[0] : e.target.value;
+        formData.set(name, value);
+        setValues({ ...values, [name]: value, formData, error: '' });
+    };
+
+    const handleBody = e => {
+        // console.log(e);
+        setBody(e);
+        formData.set('body', e);
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('blog', JSON.stringify(e));
+        }
+    };
+    const showError = () => (
+        <div className="alert alert-danger" style={{ display: error ? '' : 'none' , animation : '$fade 2s 1s infinite' }}>
+            {error}
+        </div>
+    );
+
+    const showSuccess = () => (
+        <div className="alert alert-success" style={{ display: success ? '' : 'none' , animation : '$fade 2s 1s infinite' }}>
+            {success}
+        </div>
+    );
+
+    const createBlogForm = () => {
+        return (
+            <form onSubmit={publishBlog}>
+                <div className="form-group">
+                    <label className="text-muted">Title</label>
+                    <input type="text" className="form-control" value={title} onChange={handleChange('title')} />
+                </div>
+
+                <div className="form-group">
+                    <ReactQuill
+                        modules={QuillModules}
+                        formats={QuillFormats}
+                        value={body}
+                        placeholder="Write something amazing..."
+                        onChange={handleBody}
+                    />
+                </div>
+
+                <div>
+                    <button type="submit" className="btn btn-primary">
+                        Publish
+                    </button>
+                </div>
+            </form>
+        );
+    };
+
+    return (
+        <div className="container-fluid pb-5">
+            <div className="row">
+                <div className="col-md-8">
+                    {createBlogForm()}
+                    <div className="pt-3">
+                        {showError()}
+                        {showSuccess()}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+export default withRouter(CreateBlog);
