56793ce3129a9a3e66453715b8c38aba7d0d26cd
sivanandi <59682017+sivanandi@users.noreply.github.com>
2022-05-01 15:38:05 +0530

    Assigning Models for works to added in MangoDB

---
+++ backend/models/blog.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const blogSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            trim: true,
+            min: 3,
+            max: 160,
+            required: true
+        },
+        slug: {
+            type: String,
+            unique: true,
+            index: true
+        },
+        body: {
+            type: {},
+            required: true,
+            min: 200,
+            max: 2000000
+        },
+        excerpt: {
+            type: String,
+            max: 1000
+        },
+        mtitle: {
+            type: String
+        },
+        mdesc: {
+            type: String
+        },
+        postedBy: {
+            type: String,
+        }
+    },
+    { timestamp: true }
+);
+
+module.exports = mongoose.model('Blog', blogSchema);
